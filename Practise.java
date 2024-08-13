bce216a21309928f5459ca678986b2d2b8e2067e
Suranjan kumar <ksuranjan951@gmail.com>
2024-08-14 01:13:24 +0530

    String1

---
+++ Practise.java
@@ -0,0 +1,9 @@
+public class Practise {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("virat");
+        sb.append(" kohli");
+        System.out.println(sb);
+        sb=new StringBuffer("sachin");
+        System.out.println(sb);
+    }
+}
